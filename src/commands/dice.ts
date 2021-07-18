6446c27c74073a4d28aea84af4820578fddc2c8e
Vijay Stroup <vijaystroup@gmail.com>
2021-07-18 18:35:13 -0400

    dice command

---
+++ src/commands/dice.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction } from 'discord.js';
+import { Command } from '@knighthacks/dispatch';
+
+const nToS: Record<number, string> = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+};
+
+const DiceCommand: Command = {
+  name: 'dice',
+  description: 'Roll a die to get a random number between 1 and 6',
+  async run(interaction: CommandInteraction) {
+    const number = Math.floor(Math.random() * 6) + 1;
+    await interaction.reply(`You rolled a  :${nToS[number]}:`);
+  }
+};
+
+export default DiceCommand;
