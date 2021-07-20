d4d25794fb111544ce9da440c67b3abef5c5091a
suneettipirneni <suneettipirneni@icloud.com>
2021-07-19 23:05:09 -0400

    add button to dice command

--- src/commands/dice.ts
+++ src/commands/dice.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, Message, MessageActionRow, MessageButton } from 'discord.js';
 import { Command } from '@knighthacks/dispatch';
 
 const numberToString: Record<number, string> = {
@@ -10,12 +10,34 @@ const numberToString: Record<number, string> = {
   6: 'six',
 };
 
+const rerollButton = new MessageButton()
+  .setLabel('Reroll')
+  .setCustomId('rerollButton')
+  .setStyle('PRIMARY');
+
+const row = new MessageActionRow().addComponents(rerollButton);
+
+const randNumber = () => Math.floor(Math.random() * 6) + 1; // +1 to exclude 0 and include 6
+
 const DiceCommand: Command = {
   name: 'dice',
   description: 'Roll a die to get a random number between 1 and 6',
   async run(interaction: CommandInteraction) {
-    const randNumber = Math.floor(Math.random() * 6) + 1; // +1 to exclude 0 and include 6
-    await interaction.reply(`You rolled a  :${numberToString[randNumber]}:`);
+    // Send message.
+    const message = await interaction.reply({ 
+      content: `You rolled a  :${numberToString[randNumber()]}:`, 
+      fetchReply: true,
+      components: [row],
+    }) as Message;
+
+    // Create a button collector.
+    const collector = message.createMessageComponentCollector({ componentType: 'BUTTON'});
+
+    // Listen for button interactions.
+    collector.on('collect', async (collectInteraction) => {
+      const rand = Math.floor(Math.random() * 6) + 1;
+      await collectInteraction.update({ content: `You rolled a  :${numberToString[rand]}:` });
+    });
   }
 };
 
