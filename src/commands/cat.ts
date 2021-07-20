563517d8189dd6a94deefb3b5c69cc369d2769d8
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-07-20 17:45:11 -0400

    add some buttons

--- src/commands/cat.ts
+++ src/commands/cat.ts
@@ -1,16 +1,39 @@
 import { Command } from '@knighthacks/dispatch';
 import axios from 'axios';
-import { CommandInteraction, MessageEmbed } from 'discord.js';
+import { CommandInteraction, InteractionReplyOptions, Message, MessageActionRow, MessageButton, MessageEmbed } from 'discord.js';
 import Colors from '../colors';
 
 const url = 'https://api.thecatapi.com/v1/images/search';
 
 type CatResponse = [{ url: string }];
 
+const button = new MessageButton()
+  .setCustomId('catButton')
+  .setStyle('PRIMARY')
+  .setLabel('Another one!');
+
+const row = new MessageActionRow().addComponents(button);
+
 async function getCatImage(): Promise<string | null> {
   return axios.get<CatResponse>(url)
     .then(response => response.data[0].url)
     .catch(() => null);
+}
+
+async function getMessage(): Promise<InteractionReplyOptions> {
+  // Get the image URL.
+  const catImageURL = await getCatImage();
+
+  if (!catImageURL) {
+    return { content: 'Error fetching Cat Image' };
+  }
+
+  const embed = new MessageEmbed()
+    .setTitle('Random Cat')
+    .setColor(Colors.embedColor)
+    .setImage(catImageURL);
+
+  return { embeds: [embed], components: [row] };
 }
 
 const CatCommand: Command = {
@@ -20,20 +43,15 @@ const CatCommand: Command = {
     // Defer while we fetch the image.
     await interaction.defer();
 
-    // Get the image URL.
-    const catImageURL = await getCatImage();
+    const message = await getMessage();
+    const repliedMessage = await interaction.followUp({ ...message, fetchReply: true }) as Message;
 
-    if (!catImageURL) {
-      await interaction.followUp({ content: 'Error fetching Cat Image' });
-      return;
-    }
-  
-    const embed = new MessageEmbed()
-      .setTitle('Random Cat')
-      .setColor(Colors.embedColor)
-      .setImage(catImageURL);
-      
-    await interaction.followUp({ embeds: [embed] });
+    const collector = repliedMessage.createMessageComponentCollector({ componentType: 'BUTTON' });
+    collector.on('collect', async (i) => {
+      await i.deferUpdate();
+      await i.update('hello');
+    });
+
   }
 };
 
