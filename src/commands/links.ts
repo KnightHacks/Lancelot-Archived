f8dca213fa392948bac51a2dd7242d422758563a
suneettipirneni <suneettipirneni@icloud.com>
2021-07-24 16:12:23 -0400

    remove comment

--- src/commands/links.ts
+++ src/commands/links.ts
@@ -28,25 +28,3 @@ const LinksCommand: Command = {
 };
 
 export default LinksCommand;
-
-
-
-// async def khlinks(self, ctx):
-//         links = (
-//             """[Find current events happening this week!](https://knighthacks.org/linktree)
-//             \n[Pay your dues](https://knighthacks.org/dues)\n
-//             [Give feedback on a specific workshop]"""
-//             """(https://knighthacks.org/feedback)\n
-//             [Sign up to be a Knight Hacks club member]"""
-//             """(https://knighthacks.org/membership)\n
-//             [Come to our ops meeting](https://knighthacks.org/ops)""")
-
-//         await ctx.send(
-//             f"{ctx.author.mention}"
-//         )
-//         embed = discord.Embed(color=0x7ce4f7, timestamp=ctx.message.created_at)
-//         embed.add_field(name="KnightHacks links", value=links)
-//         embed.set_thumbnail(
-//             url="https://github.com/lienne/ModiBot/blob/master/cogs/img/headphonesdog.png?raw=true"
-//         )
-//         await ctx.send(embed=embed)
