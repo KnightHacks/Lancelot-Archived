4311e5b60f6ea2af57f342b93ffd6e1834d0fb75
suneettipirneni <suneettipirneni@icloud.com>
2021-07-24 16:10:25 -0400

    add website

--- src/commands/links.ts
+++ src/commands/links.ts
@@ -2,14 +2,15 @@ import { Command } from '@knighthacks/dispatch';
 import { MessageActionRow, MessageButton } from 'discord.js';
 
 type LinkButtonData = { link: string; label: string };
-const links: Record<string, LinkButtonData> = {
-  LinkTree: { link: 'https://knighthacks.org/linktree', label: 'Link Tree' },
-  Dues: { link: 'https://knighthacks.org/dues', label: 'Pay Dues' },
-  Feedback: { link: 'https://knighthacks.org/feedback', label: 'Workshop Feedback' },
-  Ops: { link: 'https://knighthacks.org/ops', label: 'Ops Meetings' },
-};
+const links: LinkButtonData[] = [
+  { link: 'https://club.knighthacks.org/', label: 'Website' },
+  { link: 'https://knighthacks.org/linktree', label: 'Link Tree' },
+  { link: 'https://knighthacks.org/dues', label: 'Pay Dues' },
+  { link: 'https://knighthacks.org/feedback', label: 'Workshop Feedback' },
+  { link: 'https://knighthacks.org/ops', label: 'Operator Meetings' },
+];
 
-const buttons: MessageButton[] = Object.entries(links).map(([_, data]) => {
+const buttons: MessageButton[] = links.map(data => {
   return new MessageButton()
     .setLabel(data.label)
     .setStyle('LINK')
@@ -22,7 +23,7 @@ const LinksCommand: Command = {
   name: 'links',
   description: 'Gets helpful links for KnightHacks.',
   async run(interaction) {
-    await interaction.reply({ content: 'View our links here!', components: [row] });
+    await interaction.reply({ content: '**Here\'s some helpful KnightHacks links!**', components: [row] });
   }
 };
 
