6a48f085e2514fb5143a49df06dfb4ba347658b0
Robert Boyd III <rob@robertcboyd.com>
2021-07-31 23:02:14 -0400

    Refactor roles command

--- src/commands/admin/roles.ts
+++ src/commands/admin/roles.ts
@@ -1,104 +1,29 @@
-import { Command } from '@knighthacks/dispatch';
-import { Client, CommandInteraction, GuildMember, Message, MessageActionRow, MessageSelectMenu } from 'discord.js';
+import { Command, SelectMenuOptions } from '@knighthacks/dispatch';
+import { Guild, GuildMember, Role, SelectMenuInteraction } from 'discord.js';
+
 const roles = [
   { label: 'OPS', description: 'The Knight Hacks Operations Team' },
   { label: 'Python', description: 'The Python programming language.' },
-  { label: 'Java', description: 'The Java programming language.'},
+  { label: 'Java', description: 'The Java programming language.' },
   { label: 'C++', description: 'The C++ programming language.' },
   { label: 'C#', description: 'The C# programming language.' },
   { label: 'JavaScript', description: 'The JavaScript programming language.' },
   { label: 'Typescript', description: 'The TypeScript programming language.' },
-  { label: 'HTML/CSS', description: 'Static website creation/design technologies.'},
+  {
+    label: 'HTML/CSS',
+    description: 'Static website creation/design technologies.',
+  },
   { label: 'Rust', description: 'The Rust programming language.' },
   { label: 'Lua', description: 'The Lua programming language.' },
   { label: 'Linux', description: 'The Linux kernel.' },
-  { label: 'Windows', description: 'The Windows operating system.'},
+  { label: 'Windows', description: 'The Windows operating system.' },
   { label: 'MacOS', description: 'The macOS operating system.' },
   { label: 'Math', description: 'The subject of mathematics.' },
   { label: 'Physics', description: 'The subject of physics.' },
 ];
 
-const roleMenu = new MessageSelectMenu().addOptions(roles.map(role => ({
-  label: role.label,
-  description: role.description,
-  value: role.label,
-})))
-  .setCustomId('roleSelectMenu')
-  .setMaxValues(roles.length);
-
-const row = new MessageActionRow().addComponents([roleMenu]);
-
-function getRoles(client: Client, roles: string[]) {
-
-  if (!process.env.GUILD_ID) {
-    throw new Error('GUILD_ID, not set!');
-  }
-
-  const guild = client.guilds.cache.get(process.env.GUILD_ID);
-
-  const fetchedRoles = roles.map(role => {
-    const fetchedRole = guild?.roles.cache.find(discordRole => discordRole.name === role);
-  
-    if (!fetchedRole) {
-      throw new Error(`Could not fetch role: ${role}`);
-    }
-  
-    return fetchedRole;
-  });
-
-  return fetchedRoles;
-}
-
-async function removeRoles(user: GuildMember, roles: string[]) {
-  // Fetch roles by name.
-  const { client } = user;
-  
-  // Filter out roles the user doesn't currently have.
-  const rolesToRemove = getRoles(client, roles)
-    .filter(roleToRemove => user.roles.cache.has(roleToRemove.id));
-
-  await user.roles.remove(rolesToRemove);
-}
-
-async function addRoles(user: GuildMember, roles: string[]) {
-  // Fetch roles by name.
-  const { client } = user;
-
-  // Filter out roles that the user already has
-  const rolesToAdd = getRoles(client, roles)
-    .filter(roleToAdd => !user.roles.cache.has(roleToAdd.id));
-
-  await user.roles.add(rolesToAdd);
-}
-
-async function sendMessage(interaction: CommandInteraction, type: 'add' | 'remove') {
-
-  const successMsg = type === 'add' ? 'added' : 'removed';
-  const message = await interaction.reply({ 
-    content: `Pick Roles to ${type}`,
-    components: [row],
-    fetchReply: true 
-  }) as Message;
-
-
-  const collector = message.createMessageComponentCollector({ componentType: 'SELECT_MENU' });
-  collector.on('collect', async (collectInteraction) => {
-    if (!collectInteraction.isSelectMenu()) {
-      return;
-    }
-
-    await collectInteraction.defer({ ephemeral: true });
-
-    const { member, values } = collectInteraction;
-    if (type === 'add') {
-      await addRoles(member as GuildMember, values);
-    } else {
-      await removeRoles(member as GuildMember, values);
-    }
-
-    await collectInteraction.editReply({ content: `Successfully ${successMsg} selected roles.`});
-  });
-}
+const getRole = (guild: Guild, roleName: string) =>
+  guild.roles.cache.find((role) => role.name === roleName);
 
 const RolesCommand: Command = {
   name: 'roles',
@@ -107,23 +32,68 @@ const RolesCommand: Command = {
     {
       name: 'add',
       description: 'Add roles to your account',
-      type: 'SUB_COMMAND'
+      type: 'SUB_COMMAND',
     },
     {
       name: 'remove',
       description: 'Remove roles to your account',
-      type: 'SUB_COMMAND'
+      type: 'SUB_COMMAND',
     },
   ],
-  async run(interaction) {
-    const subCommand = interaction.options.getSubCommand();
-
-    if (subCommand === 'add') {
-      await sendMessage(interaction, 'add');
-    } else {
-      await sendMessage(interaction, 'remove');
+  async run({ interaction: { options, reply, guild }, registerUI }) {
+    if (!guild) {
+      await reply({
+        content: 'Unexpected null guild!',
+      });
+      return;
     }
-  }
+    const action = options.getSubcommand() as 'add' | 'remove';
+    const ui: SelectMenuOptions = {
+      options: roles.map((role) => ({
+        label: role.label,
+        description: role.description,
+        // FIXME remove this
+        value: role.label,
+      })),
+      maxValues: roles.length,
+      async onSelect({
+        defer,
+        editReply,
+        member,
+        values: roleNames,
+      }: SelectMenuInteraction) {
+        if (!member || !(member instanceof GuildMember)) {
+          await reply({
+            content: `Invalid member property: ${member}`,
+          });
+          return;
+        }
+        await defer({ ephemeral: true });
+        const roles: (Role | undefined)[] = roleNames.map((roleName) =>
+          getRole(guild, roleName)
+        );
+        const validRoles = roles.filter((role): role is Role => !!role);
+        // FIXME this error message could be better
+        if (roles.filter((role) => role === undefined).length > 0) {
+          await editReply({ content: 'Role lookup failed!' });
+          return;
+        }
+        if (action === 'add') {
+          validRoles.forEach((role) => member.roles.add(role));
+        } else {
+          validRoles.forEach((role) => member.roles.remove(role));
+        }
+        const successMsg = action === 'add' ? 'added' : 'removed';
+        await editReply({
+          content: `Successfully ${successMsg} selected roles.`,
+        });
+      },
+    };
+    await reply({
+      content: `Pick Roles to ${action}`,
+      components: registerUI(ui),
+    });
+  },
 };
 
 export default RolesCommand;
