6f08b513c7a02d1bfba28424e5fb34253554909e
suneettipirneni <suneettipirneni@icloud.com>
2021-08-04 18:01:28 -0400

    temp remove crumbl command

--- src/commands/util/crumbl.ts
+++ src/commands/util/crumbl.ts
@@ -1,37 +1,38 @@
 import { Command } from '@knighthacks/dispatch';
-import axios from 'axios';
-import { MessageEmbed } from 'discord.js';
-import { sendPaginatedEmbeds } from '../../util/paginator';
+// import axios from 'axios';
+// import { MessageEmbed } from 'discord.js';
+// import { sendPaginatedEmbeds } from '../../util/paginator';
 
-interface Cookie {
-  name: string;
-  description: string;
-  image: string;
-}
+// interface Cookie {
+//   name: string;
+//   description: string;
+//   image: string;
+// }
 
-async function fetchCookiesData(): Promise<Cookie[]> {
-  return (
-    await axios.get(
-      'https://jpswqfm3od.execute-api.us-east-1.amazonaws.com/default/crumbl-api'
-    )
-  ).data;
-}
+// async function fetchCookiesData(): Promise<Cookie[]> {
+//   return (
+//     await axios.get(
+//       'https://jpswqfm3od.execute-api.us-east-1.amazonaws.com/default/crumbl-api'
+//     )
+//   ).data;
+// }
 
 const crumbl: Command = {
   name: 'crumbl',
   description: 'View the current weekly specialty cookies at Crumbl Cookies!',
   async run({ interaction: i }) {
-    await i.defer();
-    const cookiesData = await fetchCookiesData();
+    // await i.defer();
+    // const cookiesData = await fetchCookiesData();
 
-    const embeds = cookiesData.map((c) => new MessageEmbed()
-      .setTitle(c.name)
-      .setDescription(c.description)
-      .setThumbnail(c.image));
+    // const embeds = cookiesData.map((c) => new MessageEmbed()
+    //   .setTitle(c.name)
+    //   .setDescription(c.description)
+    //   .setThumbnail(c.image));
 
-    await sendPaginatedEmbeds(i, embeds, {
-      content: 'Here are the weekly specialty cookies!'
-    });
+    // await sendPaginatedEmbeds(i, embeds, {
+    //   content: 'Here are the weekly specialty cookies!'
+    // });
+    await i.reply('Command not available.');
   }
 };
 
