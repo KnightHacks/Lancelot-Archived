798a63e96ab91d73bd68b07a7b7ad0ebac2f86b4
Suneet Tipirneni <suneettipirneni@icloud.com>
2021-08-04 17:28:43 -0400

    clean up

--- src/commands/util/poll.ts
+++ src/commands/util/poll.ts
@@ -1,5 +1,5 @@
 import { Command } from '@knighthacks/dispatch';
-import { ApplicationCommandOptionData, CommandInteractionOption, Message, MessageActionRow, MessageButton, MessageEmbed } from 'discord.js';
+import { ApplicationCommandOptionData, CommandInteractionOption, Message, MessageActionRow, MessageEmbed, MessageSelectMenu, MessageSelectOptionData, Snowflake, User } from 'discord.js';
 
 function generateChoiceOptions(numChoices: number): ApplicationCommandOptionData[] {
   const retVal: ApplicationCommandOptionData[] = [];    
@@ -22,28 +22,98 @@ function generateChoiceOptions(numChoices: number): ApplicationCommandOptionData
   return retVal;
 }
 
-function generateButtonOptions(options: CommandInteractionOption[]): MessageActionRow[] {
-  const buttons = options.map(option => new MessageButton()
-    .setCustomId(option.value as string)
-    .setLabel(option.value as string)
-    .setStyle('PRIMARY')
-  );
+export class PollManager {
+  private readonly voteMap = new Map<string, number>();
+  private readonly userVoteMap = new Map<Snowflake, string>();
+  private readonly title;
 
-  const components: MessageActionRow[] = [];
-  let curRow = 0;
-  buttons.forEach((button, i) => {
-    if (i > 4) {
-      curRow++;
+  constructor(title: string, options: CommandInteractionOption[]) {
+    // Initialize vote map
+    options.forEach(option => {
+
+      if (typeof option.value !== 'string') {
+        return;
+      }
+
+      this.voteMap.set(option.value, 0);
+    });
+
+    this.title = title;
+  }
+
+  generateEmbed(): MessageEmbed {
+    const entries = [...this.voteMap.entries()].sort((a, b) => {
+      const [, aCount] = a;
+      const [, bCount] = b;
+
+      return bCount - aCount;
+    });
+
+    return new MessageEmbed().addFields(entries.map((entry, i) => {
+      const [ optionName, voteCount ] = entry;
+      return {
+        name: `${i + 1}) ${optionName}`,
+        value: `Votes: ${voteCount}`
+      };
+    }))
+      .setTitle(this.title);
+  }
+
+  vote(user: User, choice: string): void {
+    const userVoteChoice = this.userVoteMap.get(user.id);
+
+    // The user has already voted for this
+    // ignore it.
+    if (userVoteChoice) {
+      // You can't vote for the same thing more than once.
+      if (userVoteChoice === choice) {
+        return;
+      }
+
+      // Decrement the other vote number.
+      const voteCount = this.getVoteCount(userVoteChoice);
+      this.voteMap.set(userVoteChoice, voteCount - 1);
     }
 
-    if (!components[curRow]) {
-      components[curRow] = new MessageActionRow();
+    // Set the users vote.
+    this.userVoteMap.set(user.id, choice);
+
+    // Get the total votes.
+    const voteCount = this.getVoteCount(choice);
+
+    // Increment votes.
+    this.voteMap.set(choice, voteCount + 1);
+  }
+
+  private getVoteCount(choice: string) {
+    const voteCount = this.voteMap.get(choice);
+
+    if (voteCount === undefined) {
+      throw new Error(`Could not get vote count for ${choice}`);
     }
 
-    components[curRow]?.addComponents(button);
+    return voteCount;
+  }
+
+  get results(): [choice: string, votes: number][] {
+    return [...this.voteMap.entries()];
+  }
+}
+
+function generateMenu(options: CommandInteractionOption[]): MessageSelectMenu {
+  const choices: MessageSelectOptionData[] = options.map(option => {
+    return {
+      label: option.value as string,
+      value: option.value as string,
+    };
+    
   });
 
-  return components;
+  return new MessageSelectMenu()
+    .setCustomId('pollMenu')
+    .setMaxValues(1)
+    .setPlaceholder('Pick your vote')
+    .addOptions(choices);
 }
 
 const PollCommand: Command = {
@@ -61,40 +131,62 @@ const PollCommand: Command = {
       type: 'INTEGER',
       description: 'The amount of time in minutes to run the poll for.',
       required: true,
-    }
-    ,...generateChoiceOptions(10)
+    },
+    ...generateChoiceOptions(10),
   ],
   async run(interaction) {
     const { options } = interaction;
-    const title = options.get('title', true);
-    const embed = new MessageEmbed()
-      .setTitle(`Poll: ${title.value}`);
+    const title = options.get('title', true).value as string;
+    const time = options.get('time', true).value as number;
 
-    const normalizedOptions = options.data.filter(option => typeof option.value === 'string' && (option.name !== 'title' && option.name !== 'time'));
+    if (time > 60) {
+      await interaction.reply({
+        content: 'Time cannot exceed 60 minutes',
+        ephemeral: true,
+      });
+      return;
+    }
 
-    const votes = new Map<string, number>();
+    const channel = interaction.channel;
 
-    normalizedOptions.forEach((elem, i) => {
-      if (typeof elem.value !== 'string') {
+    if (!channel) {
+      throw new Error('Could not find channel');
+    }
+
+    const normalizedOptions = options.data.filter(option => typeof option.value === 'string' 
+      && (option.name !== 'title' && option.name !== 'time'));
+
+    const poll = new PollManager(title, normalizedOptions);
+    const embed = poll.generateEmbed();
+
+    const row = new MessageActionRow()
+      .addComponents(generateMenu(normalizedOptions));
+
+    const message = await interaction.reply({ embeds: [embed], fetchReply: true, components: [row] }) as Message;
+    const collector = message.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: time * 60000 });
+
+    collector.on('collect', async (collectInteraction) => {
+      await collectInteraction.deferUpdate();
+      if (!collectInteraction.isSelectMenu()) {
         return;
       }
 
-      // Initialize map values.
-      votes.set(elem.name, 0);
-      embed.addField(`Choice #${i + 1}`, '0');
+      const [ choice ] = collectInteraction.values;
+
+      if (!choice) {
+        return;
+      }
+
+      poll.vote(collectInteraction.user, choice);
+      const updatedEmbed = poll.generateEmbed();
+
+      await collectInteraction.editReply({ embeds: [updatedEmbed] });
     });
 
-    const row = generateButtonOptions(normalizedOptions);
-
-    const message = await interaction.reply({ embeds: [embed], fetchReply: true, components: row }) as Message;
-    const collector = message.createMessageComponentCollector({ componentType: 'BUTTON' });
-
-    collector.on('collect', (collectInteraction) => {
-      if (!collectInteraction.isButton()) {
-        return;
-      }
-
-      console.log(collectInteraction.customId);
+    collector.on('end', async () => {
+      const embed = poll.generateEmbed();
+      embed.title = `Results of '${title}':`;
+      await message.reply({ content: 'Poll: has concluded:', embeds: [embed] });
     });
   }
 };
