db4b5995c58bc7b5e7025cc6f7f90dd813450ee1
suneettipirneni <suneettipirneni@icloud.com>
2021-08-06 12:23:41 -0400

    fix crumbl

--- src/commands/util/crumbl.ts
+++ src/commands/util/crumbl.ts
@@ -1,38 +1,37 @@
 import { Command } from '@knighthacks/dispatch';
-// import axios from 'axios';
-// import { MessageEmbed } from 'discord.js';
-// import { sendPaginatedEmbeds } from '../../util/paginator';
+import axios from 'axios';
+import { MessageEmbed } from 'discord.js';
+import { sendPaginatedEmbeds } from '../../util/paginator';
 
-// interface Cookie {
-//   name: string;
-//   description: string;
-//   image: string;
-// }
+interface Cookie {
+  name: string;
+  description: string;
+  image: string;
+}
 
-// async function fetchCookiesData(): Promise<Cookie[]> {
-//   return (
-//     await axios.get(
-//       'https://jpswqfm3od.execute-api.us-east-1.amazonaws.com/default/crumbl-api'
-//     )
-//   ).data;
-// }
+async function fetchCookiesData(): Promise<Cookie[]> {
+  return (
+    await axios.get(
+      'https://crumbl-api-2.rb32020.workers.dev/'
+    )
+  ).data;
+}
 
 const crumbl: Command = {
   name: 'crumbl',
   description: 'View the current weekly specialty cookies at Crumbl Cookies!',
   async run({ interaction: i }) {
-    // await i.defer();
-    // const cookiesData = await fetchCookiesData();
+    await i.deferReply();
+    const cookiesData = await fetchCookiesData();
 
-    // const embeds = cookiesData.map((c) => new MessageEmbed()
-    //   .setTitle(c.name)
-    //   .setDescription(c.description)
-    //   .setThumbnail(c.image));
+    const embeds = cookiesData.map((c) => new MessageEmbed()
+      .setTitle(c.name)
+      .setDescription(c.description)
+      .setThumbnail(c.image));
 
-    // await sendPaginatedEmbeds(i, embeds, {
-    //   content: 'Here are the weekly specialty cookies!'
-    // });
-    await i.reply('Command not available.');
+    await sendPaginatedEmbeds(i, embeds, {
+      content: 'Here are the weekly specialty cookies!'
+    });
   }
 };
 
