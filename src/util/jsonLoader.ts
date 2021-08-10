f3afd70d2438ae94ff1201e6d4b0b78592d2b283
suneettipirneni <suneettipirneni@icloud.com>
2021-08-10 00:31:55 -0400

    add presence and replies

---
+++ src/util/jsonLoader.ts
@@ -0,0 +1,7 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+export async function loadReplies(): Promise<string[]> {
+  const fileData = await fs.readFile(path.join(__dirname ,'../../replies.json'));
+  return JSON.parse(fileData.toString());
+}
