19b1a1010e71d04a501cdbabb8095560eea0ea0f
Robert Boyd III <rob@robertcboyd.com>
2021-08-11 00:13:02 -0400

    Convert loadReplies to use JSON import

--- src/util/jsonLoader.ts
+++
@@ -1,7 +0,0 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-
-export async function loadReplies(): Promise<string[]> {
-  const fileData = await fs.readFile(path.join(__dirname ,'../../replies.json'));
-  return JSON.parse(fileData.toString());
-}
