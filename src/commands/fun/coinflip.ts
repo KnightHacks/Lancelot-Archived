419e3014f0abce440b82b0dcaede57115ced277a
Robert Boyd III <rob@robertcboyd.com>
2021-08-28 23:42:40 -0400

    Remove singleButtonRow from coinflip

--- src/commands/fun/coinflip.ts
+++ src/commands/fun/coinflip.ts
@@ -1,13 +1,5 @@
-import { Message } from 'discord.js';
 import { Command, inChannelNames } from '@knighthacks/dispatch';
 import { Channels } from '../../channels';
-import { singleButtonRow } from '../../util/button';
-
-const row = singleButtonRow({
-  label: 'Flip Again',
-  style: 'PRIMARY',
-  customId: 'flipButton',
-});
 
 function getFlip(): string {
   const flip = Math.round(Math.random());
@@ -18,18 +10,16 @@ const CoinFlipCommand: Command = {
   name: 'coinflip',
   description: 'Performs a coin flip',
   permissionHandler: inChannelNames(Channels.bot),
-  async run({ interaction }): Promise<void> {
-    const message = (await interaction.reply({
+  async run({ interaction, registerUI }): Promise<void> {
+    await interaction.reply({
       content: `${interaction.user.username}, you got ${getFlip()}`,
-      fetchReply: true,
-      components: [row],
-    })) as Message;
-
-    const collector = message.createMessageComponentCollector({
-      componentType: 'BUTTON',
-    });
-    collector.on('collect', async (i) => {
-      await i.update(`${interaction.user.username}, you got ${getFlip()}`);
+      components: registerUI({
+        style: 'PRIMARY',
+        label: 'Flip Again',
+        async onClick({ update }) {
+          await update(`${interaction.user.username}, you got ${getFlip()}`);
+        },
+      }),
     });
   },
 };
