26b72c0dfe70546a4b29e325769b7200c4caaaed
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2021-09-05 16:05:21 -0400

    Update testCommand.ts

--- src/commands/util/testCommand.ts
+++ src/commands/util/testCommand.ts
@@ -13,7 +13,7 @@ const command: Command = {
   permissionHandler: inChannelNames(Channels.bot),
   async run({ interaction }): Promise<void> {
     await sendPaginatedEmbeds(interaction, [embed1, embed2, embed3], {
-      content: process.env.GUILD_ID,
+      content: process.env.GITHUB_SHA,
       nextLabel: 'Bar',
       previousLabel: 'foo',
       style: 'DANGER',
